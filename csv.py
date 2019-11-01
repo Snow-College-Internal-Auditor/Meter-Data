59aa43467f51795f9e3a57dea289b03e09d2151f
McKinnin Lloyd <mckinnin.lloyd@ad.snow.edu>
2019-11-01 00:36:40 -0600

    created vairable meter pull. Need to create array for it.

--- csv.py
+++ csv.py
@@ -1,7 +1,9 @@
 import webbrowser
 
 
-def multipleMeters():
+def setMeters():
+    meters = ['300001306', '350000381']
+
     print("Select time interval")
     print("1. 15 minute intervals")
     print("2. Hour intervals")
@@ -10,7 +12,6 @@ def multipleMeters():
     #can set this up with billing cycle. So Sep 15 - Oct 14. Talk to wayne
     print("5. Month intervals")
     userinput = input("Enter: ")
-
     interval = int(userinput)
 
     if interval == 1:
@@ -24,6 +25,40 @@ def multipleMeters():
     elif interval == 5:
         dateRange = "mo"
 
+    print("Please enter a start and end date for the summary.")
+    print("Enter date as YYYYMMDDhhmm")
+    print("Example: 201910310930 is 10/31/2019 at 9:30am")
+
+    startDate = input("Enter the start date:" )
+    endDate = input("Enter the end date: ")
+
+    for i in meters:
+        webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + i + 
+                        '&format=html&report=' + dateRange + '&limit=1000&offset=0&timezone=America~Denver&start_date=' + startDate + 
+                        '&end_date=' + endDate)
+
+
+def multipleMeters():
+    print("Select time interval")
+    print("1. 15 minute intervals")
+    print("2. Hour intervals")
+    print("3. day intervals")
+    print("4. Week intervals")
+    #can set this up with billing cycle. So Sep 15 - Oct 14. Talk to wayne
+    print("5. Month intervals")
+    userinput = input("Enter: ")
+    interval = int(userinput)
+
+    if interval == 1:
+        dateRange = "15"
+    elif interval == 2:
+        dateRange = "hr"
+    elif interval == 3:
+        dateRange = "dy"
+    elif interval == 4:
+        dateRange = "wk"
+    elif interval == 5:
+        dateRange = "mo"
 
     print("Please enter a start and end date for the summary.")
     print("Enter date as YYYYMMDDhhmm")
@@ -31,13 +66,16 @@ def multipleMeters():
 
     startDate = input("Enter the start date:" )
     endDate = input("Enter the end date: ")
-    
-    meters = ['300001306', '350000381']
 
-    for i in meters:
-        webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + i + 
-                        '&format=html&report=' + dateRange + '&limit=1000&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)
-          
+    unserinput = input("How many meters do you want to pull: ")
+    numberOfMeters = int(unserinput)
+
+    for i in range(numberOfMeters):
+        meter = input("Meter number: ")
+        webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + meter + 
+                        '&format=html&report=' + dateRange + '&limit=1000&offset=0&timezone=America~Denver&start_date=' + startDate + 
+                        '&end_date=' + endDate)
+
 
 def singalMeter():
     meterNumber = input("Please enter the meter number: ")
@@ -79,6 +117,7 @@ def singalMeter():
 def menu():
     print("1. One meter summary")
     print("2. multiple meter summary")
+    print("3. Variable meters")
     val = input("Enter number: ")
     choice = int(val)
     return choice
@@ -88,6 +127,8 @@ def switch(choice):
     if choice == 1:
         singalMeter()
     elif choice == 2:
+        setMeters
+    elif choice == 3:
         multipleMeters()          
 
 
