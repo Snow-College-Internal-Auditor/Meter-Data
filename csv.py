e7851edcfe3c6bb1535cd82e247bf2aa1dc4c3fb
McKinnin <45369137+McKinnin@users.noreply.github.com>
2019-10-31 21:41:37 -0600

    Add files via upload
    Will take user input on the date range

--- csv.py
+++ csv.py
@@ -1,18 +1,33 @@
 import webbrowser
 import datetime
-#import win32com.client as win32comclient
+
 #--------------------------------------gets current date-------------------------------#
+
 from dateutil.relativedelta import relativedelta
 currentdate = datetime.datetime.now()
 date_formated = currentdate.strftime("%m%d%Y")
+
 #--------------------------------------gets current date-------------------------------#
+
+#--------------------------------------gets date range date-------------------------------#
+
+print("Please enter a start and end date for the summary.")
+print("Enter date as YYYYMMDDhhmm")
+print("Example: 201910310930 is 10/31/2019 at 9:30am")
+startDate = input("Enter the start date:" )
+endDate = input("Enter the end date: ")
+
+#--------------------------------------gets date range date-------------------------------#
 
 
 #--------------------------------------calls api---------------------------------------#
+
 meters = ['300001306', '350000381']
 
 for i in meters:
-    #webbrowser.open('http://io.ekmpush.com/readMeter?meters=' + i + '&ver=v4&key=NjUyMjA2ODE6R2JXZEluYWw&fmt=csv&cnt=10&timezone=America~Denver&since=1572552180250')
-    webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=300001306&format=html&report=mo&limit=10&offset=0&timezone=America~Denver&start_date=201909011432&end_date=201910311432')  
+    webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + i + 
+                    '&format=csv&report=mo&limit=10&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)  
+    print("h")
+
 #--------------------------------------calls api---------------------------------------#
 
