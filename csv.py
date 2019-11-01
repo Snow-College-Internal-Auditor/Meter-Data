6837d6f8d1464cd335cc1d0afe17ecf17dea1a67
McKinnin Lloyd <mckinnin.lloyd@ad.snow.edu>
2019-10-31 23:36:51 -0600

    added if statements to change the range. Have not added it to the api yet. Only done this in one meter function.

--- csv.py
+++ csv.py
@@ -19,15 +19,38 @@ def multipleMeters():
 def singalMeter():
     meterNumber = input("Please enter the meter number: ")
 
+    print("Select time interval")
+    print("1. 15 minute intervals")
+    print("2. Hour intervals")
+    print("3. day intervals")
+    print("4. Week intervals")
+    #can set this up with billing cycle. So Sep 15 - Oct 14. Talk to wayne
+    print("5. Month intervals")
+    userinput = input("Enter: ")
+
+    interval = int(userinput)
+
+    if interval == 1:
+        dateRange = "15"
+    elif interval == 2:
+        dateRange = "hr"
+    elif interval == 3:
+        dateRange = "dy"
+    elif interval == 4:
+        dateRange = "wk"
+    elif interval == 5:
+        dateRange = "mo"
+
     print("Please enter a start and end date for the summary.")
     print("Enter date as YYYYMMDDhhmm")
     print("Example: 201910310930 is 10/31/2019 at 9:30am")
 
-    startDate = input("Enter the start date:" )
+    startDate = input("Enter the start date: " )
     endDate = input("Enter the end date: ")
     
     webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + meterNumber + 
-                    '&format=html&report=mo&limit=10&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)
+                    '&format=html&report=' + dateRange + '&limit=10000&offset=0&timezone=America~Denver&start_date=' + startDate + 
+                    '&end_date=' + endDate)
 
 
 def menu():
