c86ea64e5f3c5b3bfd2b0602524105f1bb389058
McKinnin Lloyd <mckinnin.lloyd@ad.snow.edu>
2019-10-31 23:41:53 -0600

    got if statments for range to work in api call for both functions.

--- csv.py
+++ csv.py
@@ -2,6 +2,29 @@ import webbrowser
 
 
 def multipleMeters():
+    print("Select time interval")
+    print("1. 15 minute intervals")
+    print("2. Hour intervals")
+    print("3. day intervals")
+    print("4. Week intervals")
+    #can set this up with billing cycle. So Sep 15 - Oct 14. Talk to wayne
+    print("5. Month intervals")
+    userinput = input("Enter: ")
+
+    interval = int(userinput)
+
+    if interval == 1:
+        dateRange = "15"
+    elif interval == 2:
+        dateRange = "hr"
+    elif interval == 3:
+        dateRange = "dy"
+    elif interval == 4:
+        dateRange = "wk"
+    elif interval == 5:
+        dateRange = "mo"
+
+
     print("Please enter a start and end date for the summary.")
     print("Enter date as YYYYMMDDhhmm")
     print("Example: 201910310930 is 10/31/2019 at 9:30am")
@@ -13,7 +36,7 @@ def multipleMeters():
 
     for i in meters:
         webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + i + 
-                        '&format=html&report=mo&limit=10&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)
+                        '&format=html&report=' + dateRange + '&limit=1000&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)
           
 
 def singalMeter():
