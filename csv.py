7b7f29850e305fd83709dfb8e33a51dad3de3a7c
McKinnin <45369137+McKinnin@users.noreply.github.com>
2019-10-31 22:22:57 -0600

    Add files via upload
    Put everything into functions and created a main to run it all

--- csv.py
+++ csv.py
@@ -1,33 +1,35 @@
 import webbrowser
-import datetime
-
-#--------------------------------------gets current date-------------------------------#
-
-from dateutil.relativedelta import relativedelta
-currentdate = datetime.datetime.now()
-date_formated = currentdate.strftime("%m%d%Y")
-
-#--------------------------------------gets current date-------------------------------#
 
 #--------------------------------------gets date range date-------------------------------#
 
-print("Please enter a start and end date for the summary.")
-print("Enter date as YYYYMMDDhhmm")
-print("Example: 201910310930 is 10/31/2019 at 9:30am")
-startDate = input("Enter the start date:" )
-endDate = input("Enter the end date: ")
+def getStartDate():
+    startDate = input("Enter the start date:" )
+    return startDate
+
+def getEndDate():
+    endDate = input("Enter the end date: ")
+    return endDate
 
 #--------------------------------------gets date range date-------------------------------#
 
+#--------------------------------------calls api---------------------------------------#
+def callApi(startDate, endDate):
+
+    meters = ['300001306', '350000381']
+
+    for i in meters:
+        webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + i + 
+                        '&format=html&report=mo&limit=10&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)  
 
 #--------------------------------------calls api---------------------------------------#
 
-meters = ['300001306', '350000381']
+def main():
+    print("Please enter a start and end date for the summary.")
+    print("Enter date as YYYYMMDDhhmm")
+    print("Example: 201910310930 is 10/31/2019 at 9:30am")
 
-for i in meters:
-    webbrowser.open('https://summary.ekmmetering.com/summary?key=NjUyMjA2ODE6R2JXZEluYWw&meters=' + i + 
-                    '&format=csv&report=mo&limit=10&offset=0&timezone=America~Denver&start_date=' + startDate + '&end_date=' + endDate)  
-    print("h")
+    startDate = getStartDate()
+    endDate = getStartDate()
 
-#--------------------------------------calls api---------------------------------------#
-
+    callApi(startDate, endDate)
+main()  
