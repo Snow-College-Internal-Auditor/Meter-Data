7516e7d3db83579bed57e595fa0bb929446a8973
McKinnin Lloyd <mckinnin.lloyd@ad.snow.edu>
2019-11-23 23:55:47 -0700

    Added input checks

--- csv.py
+++ csv.py
@@ -59,10 +59,11 @@ def setMeters():
     print("4. Week intervals")
     #can set this up with billing cycle. So Sep 15 - Oct 14. Talk to wayne
     print("5. Month intervals")
-    #----input validation-----
     userinput = input("Enter: ")
+    while userinput != '1' and userinput != '2' and userinput != '3' and userinput !='4' and userinput !='5':
+        print(userinput + ' is not one of the options. Please select again.')
+        userinput = input("Enter: ")
     interval = int(userinput)
-    #----input validation-----
 
     if interval == 1:
         dateRange = "15"
