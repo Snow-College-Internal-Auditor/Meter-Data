073b7785d92eb76693a63f40e33bcc8ba4eaadbb
McKinnin <45369137+McKinnin@users.noreply.github.com>
2019-12-30 06:54:01 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-repeat = 'y'
-
-while repeat == 'y':
-    repeat = input('Do you want to run another api call? [y/n]: ')
-    while repeat != 'y' and repeat != 'n':
-        print('can only enter y or n')
-        repeat = input('Do you want to run another api call? [y/n]: ')
-
